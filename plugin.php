25f25cf9025331c04719641340e61737955de767
artpi <artur.piszek@gmail.com>
2025-04-14 11:34:41 +0200

    more stuff

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Template
+ * Description: Artpi WordPress plugin template.
+ * Version: 0.0.1
+ * Author: Artpi
+ * Author URI: https://piszek.com/
+ * Text Domain: template
+ * License: GPL2
+ */
